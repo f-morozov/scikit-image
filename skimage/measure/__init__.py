4ef1582598c01fb632fda6d9a28245a8eb2d742a
Johannes Schönberger <hannesschoenberger@gmail.com>
2013-02-28 19:39:04 +0100

    Add imports of fit to subpackage

--- skimage/measure/__init__.py
+++ skimage/measure/__init__.py
@@ -2,3 +2,4 @@ from .find_contours import find_contours
 from ._regionprops import regionprops, perimeter
 from ._structural_similarity import structural_similarity
 from ._polygon import approximate_polygon, subdivide_polygon
+from .fit import LineModel, CircleModel, EllipseModel, ransac
